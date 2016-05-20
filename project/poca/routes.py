239b28c68865df11eb8667b7e1fd66387df3df15
Ross Jones <ross@servercode.co.uk>
2016-05-20 14:11:21 +0100

    Outline of dataset page

--- project/poca/routes.py
+++ project/poca/routes.py
@@ -3,4 +3,5 @@ import poca.views as v
 def create_routes(app):
     app.add_url_rule('/', 'home', v.home)
     app.add_url_rule('/search', 'search', v.search)
+    app.add_url_rule('/dataset/<name>', 'dataset', v.dataset)
 
--- project/poca/views.py
+++ project/poca/views.py
@@ -13,26 +13,38 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-
 def home():
     ctx = {}
     return render_template('index.html', **ctx)
 
 def search():
-  terms = request.args.get('q', '')
-  results = [
-    {'title': 'My shiny dataset',
-      'description': 'This dataset is an aggregation of the data found in some other datasets ',
-      'publisher': {'title': 'Cabinet Office'}
+    terms = request.args.get('q', '')
+    results = [
+    {
+        'title': 'My shiny dataset',
+        'name': 'shiny',
+        'description': 'This dataset is an aggregation of the data found in some other datasets ',
+        'publisher': {'title': 'Cabinet Office'}
     },
-    {'title': 'Another shiny dataset',
-     'description': 'There is very little data in this dataset, in fact it is a PDF file!',
-      'publisher': {'title': 'Cabinet Office'}
+    {
+        'title': 'Another shiny dataset',
+        'name': 'shiny2',
+        'description': 'There is very little data in this dataset, in fact it is a PDF file!',
+        'publisher': {'title': 'Cabinet Office'}
     },
-  ]
+    ]
 
-  ctx = {
+    ctx = {
     'terms': terms,
     'results': results,
-  }
-  return render_template('search.html', **ctx)
+    }
+    return render_template('search.html', **ctx)
+
+def dataset(name):
+    ctx = {
+        'dataset': {
+            'title': 'A shiny new dataset'
+        },
+        'publisher': {'title': 'Cabinet Office'}
+    }
+    return render_template('dataset.html', **ctx)
