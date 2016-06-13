cc33602ca47755136b41ce226d8935eb1446dcb5
Ross Jones <ross@servercode.co.uk>
2016-06-13 09:37:58 +0100

    Add new spending script, takes a while to run

--- project/poca/scripts/__init__.py
+++ project/poca/scripts/__init__.py
@@ -2,10 +2,12 @@ from .carparks import CarparkImport
 from .publishers import PublisherImport
 from .datasets import DatasetImport
 from .spend import SpendImport
+from .spend2 import Spend2Import
 
 commands = [
     CarparkImport,
     PublisherImport,
     DatasetImport,
-    SpendImport
+    SpendImport,
+    Spend2Import,
 ]
